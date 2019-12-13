78e47292ad85d4f1af0408f2ffd12835d0e14ab7
jungsuji <wtw9611@gmail.com>
2019-12-13 17:21:34 +0900

    post-list.html 생성 후 url 입력 시 나오는 화면까지 완료

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+import os
 
-# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
+
+
+def post_list(request):
+    # cur_file_path = os.path.abspath(__file__)
+    # blog_file_path = os.path.dirname(cur_file_path)
+    # root_file_path = os.path.dirname(blog_file_path)
+    #
+    # templates_file_path = os.path.join(root_file_path, 'templates')
+    # post_list_html_path = os.path.join(templates_file_path, 'post-list.html')
+    #
+    # f = open(post_list_html_path, 'rt')
+    # html = f.read()
+    # f.close()
+
+    # return HttpResponse(html)
+
+    # content = loader.render_to_string('post-list.html', None, request)
+    # return HttpResponse(content)
+
+    return render(request, 'post-list.html')
--- config/settings.py
+++ config/settings.py
@@ -15,6 +15,10 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+print('settings.py의 BASE_DIR:', BASE_DIR)
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+print('settings.py의 TEMPLATES_DIR:', TEMPLATES_DIR)
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -51,11 +55,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'config.urls'
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
